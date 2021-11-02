b5ec809ead98a42a81384d00dd112f9686310e05
Amulya Dixit <amulyadixit07@gmail.com>
2021-11-02 18:51:10 +0530

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+import Header from './../Header/Header'
+import SearchBox from '../SearchBox/SearchBox';
+
+class App extends React.Component{
+    state={
+        headerText: "Name It!",
+        headerExpanded: true
+    };
+    
+    handleInputChange=(inputText)=>{
+        // console.log('Input text is - ',inputText)
+        this.setState({headerExpanded: inputText.length>0 ? false : true});
+    };
+
+    render(){
+        return (
+        <div>
+            <Header  headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText}/>
+            <SearchBox onInputChange={this.handleInputChange}/>
+            
+            
+        </div>
+       
+        );
+    }
+}
+export default App
