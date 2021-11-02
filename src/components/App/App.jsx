6a9ed01aaa04da62ad60e19ac265f337f1a8272c
Amulya Dixit <amulyadixit07@gmail.com>
2021-11-02 20:35:32 +0530

    Integrate name Library

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import './App.css'
 import Header from './../Header/Header'
 import SearchBox from '../SearchBox/SearchBox';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
+
+const name=require('@rstacruz/startup-name-generator')
 
 class App extends React.Component{
     state={
         headerText: "Name It!",
-        headerExpanded: true
+        headerExpanded: true,
+        suggestedNames: [],
     };
     
     handleInputChange=(inputText)=>{
         // console.log('Input text is - ',inputText)
-        this.setState({headerExpanded: inputText.length>0 ? false : true});
+        this.setState({headerExpanded: inputText.length>0 ? false : true,
+            suggestedNames: name(inputText)});
     };
 
     render(){
+
         return (
         <div>
             <Header  headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText}/>
             <SearchBox onInputChange={this.handleInputChange}/>
-            
+            <ResultsContainer suggestedNames={this.state.suggestedNames}/>
             
         </div>
        
