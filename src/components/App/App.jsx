bb9a4f3fbdd04ffd3822abefb409c9669899aa2a
Amulya Dixit <amulyadixit07@gmail.com>
2021-11-03 23:14:59 +0530

    Converting class based components to functional based components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,28 @@
-import React from 'react'
-import './App.css'
-import Header from './../Header/Header'
+import React, { useState } from 'react';
+import './App.css';
+import Header from './../Header/Header';
 import SearchBox from '../SearchBox/SearchBox';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 
-const name=require('@rstacruz/startup-name-generator')
+const name = require('@rstacruz/startup-name-generator');
 
-class App extends React.Component{
-    state={
-        headerText: "Name It!",
-        headerExpanded: true,
-        suggestedNames: [],
-    };
-    
-    handleInputChange=(inputText)=>{
-        // console.log('Input text is - ',inputText)
-        this.setState({headerExpanded: inputText.length>0 ? false : true,
-            suggestedNames: inputText ? name(inputText) : []});
+const App = () => {
+    const [headerText, setheaderText] = useState('Name It!');
+    const [headerExpanded, setheaderExpanded] = useState(true);
+    const [suggestedNames, setsuggestedNames] = useState([]);
+
+    const handleInputChange = (inputText) => {
+        // setheaderExpanded: inputText.length > 0 ? false : true;
+        setheaderExpanded(inputText.length > 0 ? false : true);
+        setsuggestedNames(inputText ? name(inputText) : []);
     };
 
-    render(){
-
-        return (
+    return (
         <div>
-            <Header  headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText}/>
-            <SearchBox onInputChange={this.handleInputChange}/>
-            <ResultsContainer suggestedNames={this.state.suggestedNames}/>
-            
+            <Header headerExpanded={headerExpanded} headTitle={headerText} />
+            <SearchBox onInputChange={handleInputChange} />
+            <ResultsContainer suggestedNames={suggestedNames} />
         </div>
-       
-        );
-    }
-}
-export default App
+    );
+};
+export default App;
